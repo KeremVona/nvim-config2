bd935fb26b8939b9ce9296bfc356a5f7e1870dbe
KeremVona <keremvona@gmail.com>
2026-06-16 16:51:53 +0300

    add colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main",
+      styles = {
+        italic = true,
+        transparency = false,
+      },
+    },
+  },
+
+  {
+    "vague2k/vague.nvim",
+    name = "vague",
+    opts = {
+      transparent = false,
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
